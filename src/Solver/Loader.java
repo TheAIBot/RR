23be4388c5f4f6aff9e212da0c36cf2f1d5a0a38
TheAIBot <andreasuranus@hotmail.com>
2016-05-07 00:00:53 +0200

    updated loader

--- src/Solver/Loader.java
+++ src/Solver/Loader.java
@@ -1,49 +1,55 @@
 package Solver;
 
 import java.awt.Point;
-import java.io.BufferedReader;
 import java.io.FileReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
+import java.util.Scanner;
 
 public class Loader {
-	public static Board loadBoard(String filepath) throws IOException {
-        try (BufferedReader br = new BufferedReader(new FileReader(filepath))) {
-            int size = Integer.parseInt(br.readLine());
-            int numberOfRobots = Integer.parseInt(br.readLine());
-            
-            char[][] b = new char[size][size];
-            Point[] robots = new Point[numberOfRobots];
-            
-            Point goal = null;
-            
-            Stream<String> fisk = br.lines();
-            List<String>lol = fisk.collect(Collectors.toList());
-            for (int i = 0; i < lol.size(); i++) {
-                char[] line = lol.get(i).toCharArray();
-            	for (int j = 0; j < line.length; j++) {
-                    if (line[j] == Board.GOAL_CHAR) {
-                        goal = new Point(i, j);
-                    } else if (Character.isDigit(line[j])) {
-                        int number = Character.getNumericValue(line[j]);
-                        robots[number] = new Point(i, j);
-                    } else if (line[j] != Board.EMPTY_CHAR &&
-                               line[j] != Board.WALL_CHAR) {
-                        System.err.println("Error: Unknown character '" + line[j] + "' on line " + (i+1) + ", column " + (j+1));
-                    }
-
-                    b[i][j] = line[j];
-                }
-			}
-            
-            if (goal == null) {
-                System.err.println("Error: No goal found on board");
-            }
-            
-            return new Board(b, robots, goal);
-        }
+	
+	public static Board loadBoard(String filepath) {
+		try {
+			return loadBoard(new Scanner(new FileReader(filepath)));
+		} catch (Exception e) {
+			return null;
+		}
     }
+	
+	public static Board loadBoard(Scanner br)
+	{
+        int size = Integer.parseInt(br.nextLine());
+        int numberOfRobots = Integer.parseInt(br.nextLine());
+        
+        char[][] b = new char[size][];
+        Point[] robots = new Point[numberOfRobots];
+        Point goal = null;
+        List<String> lol = new ArrayList<String>();
+        for (int i = 0; i < size; i++) {
+			lol.add(br.nextLine());
+			b[i] = new char[size];
+		}
+        for (int i = 0; i < lol.size(); i++) {
+            char[] line = lol.get(i).toCharArray();
+        	for (int j = 0; j < line.length; j++) {
+                if (line[j] == Board.GOAL_CHAR) {
+                    goal = new Point(j, i);
+                } else if (Character.isDigit(line[j])) {
+                    int number = Character.getNumericValue(line[j]);
+                    robots[number] = new Point(j, i);
+                } else if (line[j] != Board.EMPTY_CHAR &&
+                           line[j] != Board.WALL_CHAR) {
+                    System.err.println("Error: Unknown character '" + line[j] + "' on line " + (i+1) + ", column " + (j+1));
+                }
+
+                b[j][i] = line[j];
+            }
+		}
+        
+        if (goal == null) {
+            System.err.println("Error: No goal found on board");
+        }
+        
+        return new Board(b, robots, goal);
+	}
 }
